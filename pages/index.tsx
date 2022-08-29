bbe2c826d2d87e2c1c2f8af3e7b9db4db887fa86
nikoids <95464823+nikoids@users.noreply.github.com>
2022-08-29 14:38:34 +0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import PostCard from '../components/cards/PostCard';
 import PhotoCards from '../components/home/PhotoCards';
 import NewsletterForm from '../components/NewsletterForm';
 import Layout from '../components/Layout';
+import { posts as allPosts } from '../utils/sample-data';
+import { getAllPostsForHome } from '../utils/api'
 import { GetStaticProps } from 'next';
 import { Post } from '../interfaces';
 import SocialCard from '../components/cards/SocialCard';
